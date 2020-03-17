6a82c6a13574f1de03c6d31795c0b7b2526980a9
MikeLogovi <40324123+MikeLogovi@users.noreply.github.com>
2020-03-17 09:35:04 +0100

    hgzig

--- mikecv/urls.py
+++ mikecv/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.i18n import i18n_patterns
 from . import settings
 from django.utils.translation import gettext_lazy as _
 urlpatterns = [
-    path('i18n/',include('django.conf.urls.i18n')),
+    path('i18n/',include('django.conf.urls.i18n')), 
     
 ]
 urlpatterns+=i18n_patterns(
