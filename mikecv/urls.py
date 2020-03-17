125f9b026ec826e30ddc79096da425d770048774
MikeLogovi <40324123+MikeLogovi@users.noreply.github.com>
2020-03-17 13:20:14 +0100

    hgzigk

--- mikecv/urls.py
+++ mikecv/urls.py
@@ -24,6 +24,7 @@ from django.utils.translation import gettext_lazy as _
 urlpatterns = [
     path('i18n/',include('django.conf.urls.i18n')), 
     
+    
 ]
 urlpatterns+=i18n_patterns(
     path(_('admin/'), admin.site.urls),
