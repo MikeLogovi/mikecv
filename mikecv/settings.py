1d2db3b8995fc54a32a996abddbc6500c209e697
MikeLogovi <40324123+MikeLogovi@users.noreply.github.com>
2020-03-17 13:37:40 +0100

    hgzigkoll

--- mikecv/settings.py
+++ mikecv/settings.py
@@ -142,7 +142,7 @@ if os.environ.get('ENV')=='PRODUCTION':
     STATICFILES_DIRS=(
         os.path.join(PROJECT_ROOT,'static'),
     )
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
     db_from_env=dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
